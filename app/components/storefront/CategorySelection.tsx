99d46270b9ef55345b41651bc878d132a466ff40
sosasanti_ <santiago.sosaa2002@gmail.com>
2025-03-02 11:50:32 -0300

    products route implemented

--- app/components/storefront/CategorySelection.tsx
+++ app/components/storefront/CategorySelection.tsx
@@ -13,7 +13,7 @@ export function CategoriesSelection(){
             <div className="mt-6 grid grid-cols-1 gap-y-6 sm:grid-cols-2 sm:grid-rows-2 sm:gap-x-6 lg:gap-8">
                 <div className="group aspect-w-2 aspect-h-1 rounded-xl overflow-hidden sm:aspect-w-1 sm:row-span-2">
                     <Image src={"/all.jpeg"} alt={"All products image"} className="object-cover object-center" width={600} height={600}/>
-                    <div className="bg-gradient-to-b from-transparent to-black opacity-55" />
+                    <div className="bg-gradient-to-b from-transparent to-black opacity-55 sm:absolute sm:inset-0" />
                     <div className="p-6 flex items-end">
                         <Link href="/products/all">
                             <h3 className="text-white font-semibold">All products </h3>
@@ -23,22 +23,22 @@ export function CategoriesSelection(){
                 </div>
                 
                 <div className="group aspect-w-2 aspect-h-1 rounded-lg overflow-hidden sm:relative sm:aspect-none sm:h-full">
-                    <Image src={"/men.jpeg"} alt={"Men products image"} className="object-center object-cover sm:absolute sm:inset-0 sm:w-full sm:h-full" width={600} height={600}/>
-                    <div className="bg-gradient-to-b from-transparent to-black opacity-55" />
-                    <div className="p-6 flex items-end">
-                        <Link href="/products/all">
-                            <h3 className="text-white font-semibold">All products </h3>
+                    <Image src={"/men.jpeg"} alt={"Men products image"} className="object-bottom object-cover sm:absolute sm:inset-0 sm:w-full sm:h-full" width={600} height={600}/>
+                    <div className="bg-gradient-to-b from-transparent to-black opacity-55 sm:absolute sm:inset-0" />
+                    <div className="p-6 flex items-end sm:absolute sm:inset-0">
+                        <Link href="/products/men">
+                            <h3 className="text-white font-semibold">Products for Men </h3>
                             <p className="mt-1 text-sm text-white">Shop Now</p>
                         </Link>
                     </div>
                 </div>
 
-                <div className="group aspect-w-2 aspect-h-1 rounded-xl overflow-hidden sm:relative sm:aspect-none sm:h-full">
-                    <Image src={"/all.jpeg"} alt={"All products image"} className="object-cover object-center" width={600} height={600}/>
-                    <div className="bg-gradient-to-b from-transparent to-black opacity-55" />
-                    <div className="p-6 flex items-end">
-                        <Link href="/products/all">
-                            <h3 className="text-white font-semibold">All products </h3>
+                <div className="group aspect-w-2 aspect-h-1 rounded-lg overflow-hidden sm:relative sm:aspect-none sm:h-full">
+                    <Image src={"/women.jpeg"} alt={"Men products image"} className="object-bottom object-cover sm:absolute sm:inset-0 sm:w-full sm:h-full" width={600} height={600}/>
+                    <div className="bg-gradient-to-b from-transparent to-black opacity-55 sm:absolute sm:inset-0" />
+                    <div className="p-6 flex items-end sm:absolute sm:inset-0">
+                        <Link href="/products/women">
+                            <h3 className="text-white font-semibold">Products for Women </h3>
                             <p className="mt-1 text-sm text-white">Shop Now</p>
                         </Link>
                     </div>
---
+++ app/components/storefront/ProductCard.tsx
@@ -0,0 +1,44 @@
+import { Button } from "@/components/ui/button";
+import { Carousel, CarouselContent, CarouselItem, CarouselNext, CarouselPrevious } from "@/components/ui/carousel";
+import Image from "next/image";
+import Link from "next/link";
+
+interface iAppProps {
+    item: {
+        id: string;
+        name: string;
+        description: string;
+        price: number;
+        images: string[];
+    }
+}
+
+export function ProductCard({item}:iAppProps) {
+    return (
+        <div className="rounded-lg">
+            <Carousel className="w-full mx-auto">
+                <CarouselContent>
+                    {item.images.map((item, index)=>(
+                        <CarouselItem key={index}>
+                            <div className="relative h-[330px]">
+                                <Image src={item} alt="Product Image" fill className="object-cover object-center w-full h-full rounded-lg"/>
+                            </div>
+                        </CarouselItem>
+                    ))}
+                </CarouselContent>
+                <CarouselPrevious className="ml-16"/>
+                <CarouselNext className="mr-16"/>
+            </Carousel>
+            <div className="flex justify-between items-center mt-2">
+                <h1 className="text-xl font-semibold">{item.name}</h1>
+                <h3 className="inline-flex items-center rounded-md bg-primary/10 px-2 py-01 text-xs font-medium text-primary ring-inset ring-primary/10">${item.price}</h3>
+            </div>
+            <p className="text-gray-600 text-sm mt-2 line-clamp-2">{item.description}</p>
+            <Button className="w-full mt-5" asChild>
+                <Link href={`/products/${item.id}`}>
+                    Learn more
+                </Link>
+            </Button>
+        </div>
+    );
+}
